717f7aa40936788b26cf45c7a87b0ae5fef9029e
OmegaTesfay <55719497+OmegaTesfay@users.noreply.github.com>
2020-03-12 10:28:21 -0700

    Create joins

---
+++ Joins.sql
@@ -0,0 +1,17 @@
+// joins carPart and seller in pictures  
+SELECT Pictures.PicturesID, Sallers.FirstName, Sallers.zipcode, CarPart.TypeOfPart, Pictures.Photo, CarPart.Cost
+FROM ((Pictures
+INNER JOIN Sallers ON Sallers.SallersID = Sallers.SallersID)
+INNER JOIN CarPart ON CarPart.CarPartID = CarPart.CarPartID);
+
+// joins carpart in Partner
+
+SELECT Partner.PartnerID, Partner.CampanyName, Partner.Campanyreview,CarPart.TypeOfPart, CarPart.Cost
+FROM Partner
+INNER JOIN CarPart ON CarPart.CarPartID=CarPart.CarPartID;
+
+
+
+
+
+
